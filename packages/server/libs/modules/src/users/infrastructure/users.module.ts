05407cbcde0fdb74a8666fbec7293f18d3ee88ee
Kami-Juan <juan1910@live.com.mx>
2020-04-12 23:39:28 -0500

    feat: added new libs for server

---
+++ packages/server/libs/modules/src/users/infrastructure/users.module.ts
@@ -0,0 +1,8 @@
+import { Module } from '@nestjs/common';
+import { UsersController } from './users.controller';
+
+@Module({
+  imports: [],
+  controllers: [UsersController],
+})
+export class UserModule {}
